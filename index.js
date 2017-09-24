9591f19cc093d940b3b6d9a178446a8743229d4b
Davi <davi.aragao@cpb.com.br>
2017-09-24 11:06:25 -0300

    Get start node heroku.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var admin = require("firebase");
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-var serviceAccount = require("primos-23a93-firebase-adminsdk-zkdtf-95b72f7641.json");
+app.set('port', (process.env.PORT || 5000));
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://primos-23a93.firebaseio.com"
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.render('pages/index')
 });
 
-firebase.database().ref('/').set({
-	username: "test",
-	email: "test@mail.com"
+app.get('/cool', function(request, response) {
+	response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
 });
