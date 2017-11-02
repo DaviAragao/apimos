3c7bc6fd57b584b22422335831b547b99676e2be
Davi <davi.aragao@cpb.com.br>
2017-11-02 18:26:49 -0200

    Adicionando novos números primos a cada requisição GETPrime.

--- api/controllers/primosController.js
+++ api/controllers/primosController.js
@@ -10,11 +10,53 @@ const getResult = (error = null, data = null) => {
 
 const now = () => new Date().toLocaleString();
 
+const addPrime = rootRef => {
+	getLastPrime(rootRef)
+		.then(prime => {
+			let newPrimeObject = new Object();
+			let nextPrime = calcNextPrime(prime);
+			newPrimeObject[nextPrime] = {
+				'calc': 0
+			};
+			rootRef.update(newPrimeObject);
+		})
+		.catch(error => console.error(error));
+};
+
+const getLastPrime = rootRef => {
+	return new Promise((resolve, reject) => {
+		rootRef.limitToLast(1).once('value')
+			.then(data => resolve(Number(extractPrimeFromJson(data.val()))))
+			.catch(error => reject(error));
+	});
+};
+
+const calcNextPrime = currentLastPrime => {
+	let candidate = currentLastPrime + 2;
+	while (!isPrime(candidate))
+		candidate += 2;
+
+	return candidate;
+};
+
+const isPrime = number => {
+	const sqrt = Math.sqrt(number);
+	let start = 2;
+
+	while (start <= sqrt)
+		if (number % start++ < 1)
+			return false;
+
+	return number > 1;
+};
+
+const extractPrimeFromJson = json => Object.keys(json)[0];
+
 exports.getNextPrime = (req, res) => {
 	const ref = db.conn.database().ref();
 	ref.orderByChild("calc").equalTo(0).limitToFirst(1).once('value')
 		.then(data => {
-			const nextPrime = Object.keys(data.val())[0];
+			const nextPrime = extractPrimeFromJson(data.val());
 			const nextPrimeRef = ref.child(nextPrime);
 			nextPrimeRef.update({
 				"calc": 1,
@@ -25,6 +67,7 @@ exports.getNextPrime = (req, res) => {
 			
 		})
 		.catch(error => res.json(getResult(error)));
+		addPrime(ref);
 };
 
 exports.updatePrime = (req, res) => {
